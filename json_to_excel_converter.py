8a50ea928c43a9f92f8a8aada02a4d687edd982d
Joel O. Wembo <JOWembo@TCH-M-JOWembo.local>
2022-12-27 13:35:54 +0800

    updated channels script

--- json_to_excel_converter.py
+++ json_to_excel_converter.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from pandas.io.json import json_normalize #package for flattening json in pandas df
 
+
 df_json = pd.read_json('data/channels_bank_list.json')
 data = pd.DataFrame(df_json)
 data = json_normalize(data['data'])
@@ -9,12 +10,8 @@ data.drop(['outlets', 'notes', 'coverage', 'turnAroundTime', 'imageSmall', \
 'feePercentage' , 'pdaxFeePercentage' , 'feeStructure' , 'pdaxFeeStructure' , \
 'feeMode' ,  'pdaxFeeMode', 'banks', 'paymentMode',  'isDisabled', 'isUnavailable' , 'featureRank' , 'platform','createdAt', 'updatedAt'], inplace=True, axis=1)
 
-# Add extra colum
-
 data.insert(8 , 'instapay bank', '', True)
 data.insert(9 , 'status', '', True)
 data.insert(10 , 'transferType', '', True)
 data.insert(11 , 'bankCode', '', True)
 data.to_excel('data/channels_bank_list.xlsx')
-
-
