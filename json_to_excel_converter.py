05bcffb381524077935c09b4c058cbcde047b5ac
Joel O. Wembo <JOWembo@TCH-M-JOWembo.local>
2022-12-27 13:21:50 +0800

    updated json to excel

--- json_to_excel_converter.py
+++ json_to_excel_converter.py
@@ -1,6 +1,9 @@
 import pandas as pd
+from pandas.io.json import json_normalize #package for flattening json in pandas df
+
 df_json = pd.read_json('data/channels_bank_list.json')
-data = pd.DataFrame (df_json)
+data = pd.DataFrame(df_json)
+data = json_normalize(data['data'])
 data.drop(['outlets', 'notes', 'coverage', 'turnAroundTime', 'imageSmall', \
 'imageMedium', 'limitMin', 'limitMax','feeAmount' , 'pdaxFeeAmount', \
 'feePercentage' , 'pdaxFeePercentage' , 'feeStructure' , 'pdaxFeeStructure' , \
@@ -13,4 +16,5 @@ data.insert(9 , 'status', '', True)
 data.insert(10 , 'transferType', '', True)
 data.insert(11 , 'bankCode', '', True)
 data.to_excel('data/channels_bank_list.xlsx')
+
 
