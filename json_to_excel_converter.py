6d95e0134f2ec23ce806cd1e3119116ba88f7e30
Joel O. Wembo <JOWembo@TCH-M-JOWembo.local>
2022-12-27 13:00:02 +0800

    script converting json to new excel sheet

--- json_to_excel_converter.py
+++ json_to_excel_converter.py
@@ -1,3 +1,16 @@
 import pandas as pd
 df_json = pd.read_json('data/channels_bank_list.json')
-df_json.to_excel('data/channels_bank_list.xlsx')
+data = pd.DataFrame (df_json)
+data.drop(['outlets', 'notes', 'coverage', 'turnAroundTime', 'imageSmall', \
+'imageMedium', 'limitMin', 'limitMax','feeAmount' , 'pdaxFeeAmount', \
+'feePercentage' , 'pdaxFeePercentage' , 'feeStructure' , 'pdaxFeeStructure' , \
+'feeMode' ,  'pdaxFeeMode', 'banks', 'paymentMode',  'isDisabled', 'isUnavailable' , 'featureRank' , 'platform','createdAt', 'updatedAt'], inplace=True, axis=1)
+
+# Add extra colum
+
+data.insert(8 , 'instapay bank', '', True)
+data.insert(9 , 'status', '', True)
+data.insert(10 , 'transferType', '', True)
+data.insert(11 , 'bankCode', '', True)
+data.to_excel('data/channels_bank_list.xlsx')
+
